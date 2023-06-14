199611c3738c20bff1846dc0d535eccd09b6ee3d
w1llow1sp <mari.front.74@gmail.com>
2023-06-14 12:53:45 +0500

    add Car Catalogue (with some strange bugs with types in Headless UI)

--- components/index.ts
+++ components/index.ts
@@ -2,11 +2,17 @@ import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+import CustomFilter from '@/components/CustomFilter';
+import SearchBar from '@/components/SearchBar';
+import SearchManufacturer from '@/components/SearchManufacture';
 
 
 export {
     Hero,
     CustomButton,
     Footer,
-    Navbar
+    Navbar,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
